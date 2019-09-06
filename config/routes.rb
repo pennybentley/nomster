46404b9ba32236dd30710f7ba389807190fdd90a
penny bentley <penny.bentley@cox.net>
2019-09-06 21:48:41 +0000

    Lesson 10.39

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places
   
   resources :places do
     resources :comments, only: :create
+  end
+
+  resources :places do
+    resources :photos, only: :create
   end
 
 end
