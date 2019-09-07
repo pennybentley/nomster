f4a1831205a0ff8814603afe514a260818e5b759
penny bentley <penny.bentley@cox.net>
2019-09-07 01:50:29 +0000

    Lesson 10.39c

--- db/migrate/20190906163526_update_photos_with_user.rb
+++ db/migrate/20190906163526_update_photos_with_user.rb
@@ -1,8 +1,15 @@
 class UpdatePhotosWithUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :photos, :user_id, :integer
-    add_index :photos, :user_id
+    create_table :photos do |t|
+      t.text :caption
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
+    end
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
   end
 end
+
 
 
