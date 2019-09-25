9286ff9326e9fa505fcea75377056bd007e5aa67
penny bentley <penny.bentley@cox.net>
2019-09-25 01:16:11 +0000

    Lesson 13.47

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   
   resources :places do
     resources :comments, only: :create
-  end
-
-  resources :places do
     resources :photos, only: :create
   end
+
+  resources :users, only: :show
 
 end
